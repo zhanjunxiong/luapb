e01c1a320561c6ec1619d44ed1db5f5e6639e708
zhanjunxiong <zhanjunxiong@126.com>
2014-09-05 15:25:10 +0800

    Update test1.lua
    [修改] 测试用例

--- test/test1.lua
+++ test/test1.lua
@@ -17,18 +17,26 @@ msg.uid = 12345;
 msg.param = 9876;
 msg.param1 = "zjx";
 --print("type: " .. type(msg.param2));
-msg.param2.add("first");
-msg.param2.add("second");
-
-msg.param2.add("three");
+msg.param2:add("first");
+msg.param2:add("second");
+msg.param2:add("three");
 
 print("uid: " .. msg.uid);
 print("param: " .. msg.param);
 print("param1: " .. msg.param1);
 
-for i = 1, msg.param2.len() do
-	local value = msg.param2[i];
+
+for i = 1, msg.param2:len() do
+	local value = msg.param2:get(i);
 	print("i: " .. i .. " value: " .. value); 
 end
+
+msg.param2[1] = "test"
+print("===== param2: " .. msg.param2:get(1))
+
+msg.param2:set(2, "test2")
+print("===== param2: " .. msg.param2:get(2))
+
+
 print("str: " .. pb.tostring(msg));
-print("hello world");
+
