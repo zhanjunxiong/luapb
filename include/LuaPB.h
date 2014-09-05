e3dcf859e5a587e761ae31657bdcbee42996e35c
zhanjunxiong <zhanjunxiong@126.com>
2014-09-05 15:22:54 +0800

    Update LuaPB.h
    [修改] luapb的实现方式

--- include/LuaPB.h
+++ include/LuaPB.h
@@ -14,15 +14,14 @@ typedef struct tagluamsg
 typedef struct tagluarepeatedmsg
 {
 	google::protobuf::Message* msg;
-	google::protobuf::FieldDescriptor *field;
+	google::protobuf::FieldDescriptor* field;
 }lua_repeated_msg;
 
-#define PB_MESSAGE_META "pb_meta"
 #define PB_MESSAGE "pb"
+#define PB_MESSAGE_META "pb_meta"
 
 #define PB_REPEATED_MESSAGE_META "pb_repeated_meta"
 
-extern "C" int push_message(lua_State* L, google::protobuf::Message* message, bool del=false);
 extern "C" int luaopen_luapb(lua_State* L);
 
 #endif
